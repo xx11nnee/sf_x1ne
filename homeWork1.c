7ff28258c36f1913aad8645a00a325d8b44d52cb
Sergey <bespalko0200@gmail.com>
2026-08-13 18:39:34 +0500

    created File Homework

---
+++ homeWork1.c
@@ -0,0 +1,61 @@
+
+#include <stdio.h>
+
+void cinemaCity()
+{
+    printf("================================\n");
+    printf("CINEMA CITY\n");
+    printf("================================\n");
+}
+
+void move()
+{
+    printf("Interstellar\n");
+}
+
+void session()
+{
+    printf("Date: 15.08.2026\n");
+    printf("Time: 19:30\n");
+}
+
+void seat()
+{
+    printf("Row: 5\n");
+    printf("Seat: 12\n");
+}
+
+void snacks()
+{
+    printf("Large Popcorn\n");
+    printf("Coca-Cola\n");
+}
+
+void price()
+{
+    printf("Ticket: 12 EUR\n");
+    printf("Snacks: 8 EUR\n");
+    printf("Total: 20 EUR\n");
+}
+
+void enjoyTheMovie()
+{
+    printf("================================\n");
+    printf(" ENJOY THE MOVIE!\n");
+    printf("================================\n");
+}
+
+
+
+int main()
+{
+    cinemaCity();
+    move();
+    session();
+    seat();
+    snacks();
+    price();
+    enjoyTheMovie();
+
+    return 0;
+}
